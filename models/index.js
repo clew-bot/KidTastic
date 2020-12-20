985418fc4f0beb6c59d5c109325f08fcaa749882
Mark Shelgren <markshelgren@outlook.com>
2020-12-19 18:23:25 -0800

    Upload first model definitions

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require("./User");
+const Child = require("./Child");
+
+User.hasMany(Child, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Child.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Child };
