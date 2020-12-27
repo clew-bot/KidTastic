c0c33cbdf8dfe5e8a64a3139d9fa8c33428a16d5
Mark Shelgren <markshelgren@outlook.com>
2020-12-27 13:59:59 -0800

    Fixing models

--- controllers/homePageRoute.js
+++ controllers/homePageRoute.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { Child, User } = require("../models");
 
 router.get("/", async (req, res) => {
   res.render("homeland");
@@ -7,16 +8,16 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     // Get all projects and JOIN with user data
+    console.log("User ID passed in: " + req.params.id);
     const childData = await Child.findAll({
       include: [
         {
           model: User,
-          attributes: ["firstname"],
         },
       ],
-      where: { userId: req.params.id },
+      // where: { userId: req.params.id },
     });
-
+    console.log("After reading +", childData);
     // Serialize data so the template can read it
     const children = childData.map((child) => child.get({ plain: true }));
 
@@ -26,6 +27,7 @@ router.get("/:id", async (req, res) => {
       loggedIn: true,
     });
   } catch (err) {
+    console.log("Error message", err);
     res.status(500).json(err);
   }
 });
--- models/User.js
+++ models/User.js
@@ -57,7 +57,6 @@ User.init(
     sequelize,
     timestamps: false,
     freezeTableName: true,
-    underscored: true,
     modelName: "user",
   }
 );
